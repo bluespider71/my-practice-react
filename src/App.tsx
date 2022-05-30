d2b1e65296b129ece03df3c03a40e54dc5ba82c9
bluespider71 <Jianglin9171@gmail.com>
2022-05-30 06:36:32 -0700

    react-props-validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
 import React from 'react';
-import JTP from './JTP ';
+import PropTypes from 'prop-types';
 
 function App(props: any) {
-  // const { name } = props;
-  const [displayBio, setdisplayBio] = React.useState(false);
-  const [name, setname] = React.useState("JavaTpoint");
-  const toggleDisplayBio = () => {
-    setdisplayBio(!displayBio);
-  }
+  const { propArray, propBool, propString, propNumber, propFunc } = props;
 
-  const bio = displayBio ? <div>
-    <h3>Javatpoint is one of the best Java training institute in Noida, Delhi, Gurugram, Ghaziabad and Faridabad. We have a team of experienced Java developers and trainers from multinational companies to teach our campus students.</h3>
-    <button onClick={toggleDisplayBio}> Show Less </button>
-  </div> : (
-    <div>
-      <button onClick={toggleDisplayBio}> Read More </button>
-    </div>
-  );
-  return (
-    <>
-      <h1> Welcome to JavaTpoint!! </h1>
-      {bio}
-      <>{name}</>
-      <JTP jtpProp={name}></JTP>
-    </>
-  );
+  return <>
+    <h1>ReactJS Props validation example</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Value</th>
+          <th>Valid</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Array</td>
+          <td>{propArray}</td>
+          <td>{propArray ? "true" : "False"}</td>
+        </tr>
+        <tr>
+          <td>Boolean</td>
+          <td>{propBool ? "true" : "False"}</td>
+          <td>{propBool ? "true" : "False"}</td>
+        </tr>
+        <tr>
+          <td>Function</td>
+          <td>{propFunc(5)}</td>
+          <td>{propFunc(5) ? "true" : "False"}</td>
+        </tr>
+        <tr>
+          <td>String</td>
+          <td>{propString}</td>
+          <td>{propString ? "true" : "False"}</td>
+        </tr>
+        <tr>
+          <td>Number</td>
+          <td>{propNumber}</td>
+          <td>{propNumber ? "true" : "False"}</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
 }
 
-// App.defaultProps = {
-//   name: "JavaTpoint"
-// }
+App.propTypes = {
+  propArray: PropTypes.array.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propFunc: PropTypes.func,
+  propNumber: PropTypes.number,
+  propString: PropTypes.string,
+}
+
+App.defaultProps = {
+  propArray: [1, 2, 3, 4, 5],
+  propBool: true,
+  propFunc: function (x: any) { return x + 5 },
+  propNumber: 1,
+  propString: "JavaTpoint",
+}
+
 
 export default App;
