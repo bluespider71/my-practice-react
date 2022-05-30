3df19191a9c1c78200b8ea318c3e8f62634f024c
bluespider71 <Jianglin9171@gmail.com>
2022-05-30 06:07:38 -0700

    react-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import React from 'react';
+
 function App() {
-  const myStyle = {
-    fontSize: 80,
-    fontFamily: "Courier",
-    color: "#003300"
+  const [displayBio, setdisplayBio] = React.useState(false);
+
+  const toggleDisplayBio = () => {
+    setdisplayBio(!displayBio);
   }
-  return (
+
+  const bio = displayBio ? <div>
+    <h3>Javatpoint is one of the best Java training institute in Noida, Delhi, Gurugram, Ghaziabad and Faridabad. We have a team of experienced Java developers and trainers from multinational companies to teach our campus students.</h3>
+    <button onClick={toggleDisplayBio}> Show Less </button>
+  </div> : (
     <div>
-      <h1 style={myStyle}>www.javatpoint.com</h1>
-      {/* This is a comment in JSX */}
+      <button onClick={toggleDisplayBio}> Read More </button>
     </div>
+  );
+  return (
+    <>
+      <h1> Welcome to JavaTpoint!! </h1>
+      {bio}
+    </>
   );
 }
 
