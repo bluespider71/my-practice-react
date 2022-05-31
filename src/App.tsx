05670556ba29caa8a9fe85616563698ae5a0ef32
bluespider71 <Jianglin9171@gmail.com>
2022-05-31 02:16:45 -0700

    react-life-cycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useEffect, useState, useRef } from 'react';
+// import PropTypes from 'prop-types';
 
 function App(props: any) {
-  const { propArray, propBool, propString, propNumber, propFunc } = props;
+  const [constructorHasRun, setConstructorHasRun] = useState(false);
+  const didMountRef = useRef(false);
+  const constructor = () => {
+    if (constructorHasRun) return;
+    console.log('Component Will MOUNT!')
+    setConstructorHasRun(true);
+  }
+  constructor();
 
+  const [hello, sethello] = useState(1)
+  const { tmpname } = props;
+
+  useEffect(() => {
+    console.log('Component Did MOUNT!')
+
+    return () => {
+      console.log("Component will UNMOUNT!")
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log('Component Will Recieve Props!', tmpname)
+  }, [tmpname])
+
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.log('Component Did UPDATE!')
+    } else {
+      didMountRef.current = true;
+    }
+  })
+  /////////////////////////////////////////////
+  //shouldComponentUpdate to React Hooks
+  // React.memo()
+  // useMemo()
+  /////////////////////////////////////////////
+  // ??componentWillUpdate
+
+
+  const changeState = (e: any) => {
+    sethello(hello + 1);
+  }
   return <>
-    <h1>ReactJS Props validation example</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Value</th>
-          <th>Valid</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Array</td>
-          <td>{propArray}</td>
-          <td>{propArray ? "true" : "False"}</td>
-        </tr>
-        <tr>
-          <td>Boolean</td>
-          <td>{propBool ? "true" : "False"}</td>
-          <td>{propBool ? "true" : "False"}</td>
-        </tr>
-        <tr>
-          <td>Function</td>
-          <td>{propFunc(5)}</td>
-          <td>{propFunc(5) ? "true" : "False"}</td>
-        </tr>
-        <tr>
-          <td>String</td>
-          <td>{propString}</td>
-          <td>{propString ? "true" : "False"}</td>
-        </tr>
-        <tr>
-          <td>Number</td>
-          <td>{propNumber}</td>
-          <td>{propNumber ? "true" : "False"}</td>
-        </tr>
-      </tbody>
-    </table>
+    <h1>ReactJS component's Lifecycle</h1>
+    <h3>Hello {hello}</h3>
+    <button onClick={changeState}>Click Here!</button>
   </>
 }
 
-App.propTypes = {
-  propArray: PropTypes.array.isRequired,
-  propBool: PropTypes.bool.isRequired,
-  propFunc: PropTypes.func,
-  propNumber: PropTypes.number,
-  propString: PropTypes.string,
-}
 
 App.defaultProps = {
-  propArray: [1, 2, 3, 4, 5],
-  propBool: true,
-  propFunc: function (x: any) { return x + 5 },
-  propNumber: 1,
-  propString: "JavaTpoint",
+  "tmpname": "name"
 }
+
 
 
 export default App;
